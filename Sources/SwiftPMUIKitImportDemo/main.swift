0a05b13b19534eb8392e162b06dd753792666c9f
noppefoxwolf <noppelabs@gmail.com>
2021-11-23 14:50:34 +0900

    add urls method call

--- Sources/SwiftPMUIKitImportDemo/main.swift
+++ Sources/SwiftPMUIKitImportDemo/main.swift
@@ -4,3 +4,6 @@ let image = UIImage(named: "Resource/image.png", in: .module, with: nil)
 
 print("Hello world!") //Hello world!
 print(image?.size) //Optional((400.0, 400.0))
+
+let urls = Bundle.module.urls(forResourcesWithExtension: "png", subdirectory: "Resource")
+print(urls?.count) // 1
